b47925952861e3c782ff2334325173f1e8a4ab43
Mohammed-3bgarino0-lgtm <Mohammed-alsakran@hotmail.com>
2026-07-26 11:22:29 +0300

    Select Google Drive image storage when configured

---
+++ src/lib/story-image-storage-factory.ts
@@ -0,0 +1,25 @@
+import { FileStoryImageStorage, type StoryImageStorage } from './story-image-storage';
+import {
+  GoogleDriveStoryImageStorage,
+  isGoogleDriveStorageConfigured,
+} from './google-drive-storage';
+import { GoogleDriveClient } from './google-drive-client';
+
+export interface StoryImageStorageSelection {
+  storage: StoryImageStorage;
+  type: 'google-drive' | 'server-filesystem';
+}
+
+export function createStoryImageStorage(): StoryImageStorageSelection {
+  const client = new GoogleDriveClient();
+  if (isGoogleDriveStorageConfigured(client)) {
+    return {
+      storage: new GoogleDriveStoryImageStorage(client),
+      type: 'google-drive',
+    };
+  }
+  return {
+    storage: new FileStoryImageStorage(),
+    type: 'server-filesystem',
+  };
+}
